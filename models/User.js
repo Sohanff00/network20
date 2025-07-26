b45567255267f037b1f404efe3cd409cc41b1379
Vexylon <140179549+Sohanff00@users.noreply.github.com>
2025-07-26 19:17:41 +0600

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  fullname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  password: { type: String, required: true },
+  referral_code: { type: String },
+  referred_by: { type: String },
+  created_at: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
